dc943ca02bba91de71e972f6e05d148d56dba2e4
Harun Gunes <h.gunes13@gmail.com>
2021-05-25 22:41:15 +0200

    Initial Commit

---
+++ CountryAndCapital/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryAndCapital
+//
+//  Created by Harun Gunes on 25/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
